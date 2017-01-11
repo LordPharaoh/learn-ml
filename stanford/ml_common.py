8ee927ccbfcfb5dbfbfd78b1ebc4c8f8b24996cc
LordPharaoh <vi.mail@protonmail.ch>
2017-01-10 21:42:26 -0800

    Gradient more all-purpose and locally weighted reg.
    Gradient now handles constant multiplication and weights (e.g. step size, lwr). Added a weight function parameter on regression.linear
    and a weight-function generating function so linear reg can use multiple weights. Linear regression has also been documented.

--- stanford/ml_common.py
+++ stanford/ml_common.py
@@ -80,36 +80,38 @@ class Training_Set:
 			batch.append(get_example(i + rand))
 		return batch
 				
-#TODO gradient can be optimized by taking it once, multiplying by each feature, and then returning the whole thing 
-def gradient(training_set, hypothesis, feature, examples):
-	"""Return the gradient of a number of examples in the training set with a given hypothesis function.
+#TODO gradient can be optimized by taking it once, multiplying by each feature, and then returning the whole thing as a list
+#contd. Getting the mean of the whole thing seemed to work, but it may give bad results with large training sets and small batch sizes
+#so basically have an option to return a list of gradients for all features, this will be faster too
+def gradient(training_set, hypothesis, feature, examples, step_size, weight_function=lambda xi, feature: 1):
+	"""Return the gradient of a number of examples in the training set with a given hypothesis function, step size, and weight.
 	
 	Keyword Arguments:
 	training_set -- the training set
 	hypothesis -- the prediction function. Should take in a list of inputs and return a prediction.
 	feature -- the feature number 
 	examples -- a list of examples in the training set to try it on; e.g. [1, 3, 7] will try it on the 1st, 3rd, and 7th training examples
+	step_size -- constant to reduce the gradient by
+	weight_function -- weight test points, change for locally weighted regression and other stuff
 	Gradient 
 		The sum of the difference of the predicted value (hypothesis) and the actual output (y) muliplied by the value of the input feature.
 		j = iteration value of the sum
 		i = feature
-		sum((h(x[j]) - y[j]) * x[j][i])
-
-	>>> ts = ml.generate_training_set(lambda x: 4 * x, 10, -10, 10, 0)
+		sum((h(x[j]) - y[j]) * x[j][i]) * weight * step_size
+	>>> ts = ml.generate_training_set(lambda x: 4*x, 10, -10, 10, variance=0)
 	>>> hypothesis = lambda x: 3 * x[0]
-	>>> ml.gradient(ts, hypothesis, 0, range(ts.size() - 1))
-	-17.937881085209728
+	>>> ml.gradient(ts, hypothesis, 0, range(ts.size() - 1), .1)
+	-3.4423139184132863
 	>>> hypothesis = lambda x: 4 * x[0]
-	>>> ml.gradient(ts, hypothesis, 0, range(ts.size() - 1))
+	>>> ml.gradient(ts, hypothesis, 0, range(ts.size() - 1), .1)
 	0.0
-
+	>>> 
 	"""
 
 	total = 0
 	for i in examples:
-		training_set.get_example(i)
 		grad = hypothesis(training_set.get_input(i)) - training_set.get_output(i)
-		total += (grad * training_set.get_input_feature(i, feature))
+		total += (grad * training_set.get_input_feature(i, feature) * weight_function(training_set.get_input(i), feature) * step_size)
 	#don't divide by zero, the answer will be 0 anyway, just divide by one
 	return (total/max(len(examples), 1))
 
--- stanford/regression.py
+++ stanford/regression.py
@@ -1,4 +1,6 @@
 import ml_common as ml
+from random import randint
+from math import exp
 
 #error (not gradient)
 def lms_error(training_set, hypothesis):
@@ -7,33 +9,78 @@ def lms_error(training_set, hypothesis):
 		total += (hypothesis(training_set.get_input(i)) - training_set.get_output(i)) ** 2
 	return total / (2 * training_set.size())
 
-def linear(training_set, step_size, num_steps, batch_size=80, log_level=ml.NORMAL, log_frequency=100, plot_error=""):
+def locally_weighted(x, bandwidth, step_size=.01):
+	"""Returns a weight function for a given x input and a bandwidth.
+	
+	Keyword Arguments:
+	x -- list of numbers, location to find line fpr
+	bandwidth -- tau, controls width of bell
+
+	Return Keyword Arguments:
+	xi -- list of numbers, input from training set
+	feature -- feature number being tested
+	"""
+	weight = lambda xi, feature: exp((-(xi[feature] - x[feature])**2)/(2*bandwidth**2))
+	return lambda training_set, hypothesis, i, examples: ml.gradient(training_set, hypothesis, i, examples, step_size, weight)
+
+def constant_weight(*args):
+	return 1
+
+def batch_gradient_descent(step_size=.01):
+	return lambda training_set, hypothesis, i, examples: ml.gradient(training_set, hypothesis, i, examples, step_size, constant_weight)
+	
+def linear(training_set, 
+			num_steps=100, 
+			batch=50,
+			update_rule=batch_gradient_descent(),
+			log_level=ml.NORMAL, 
+			log_frequency=100):
+	"""Returns a function that accepts a list of inputs and returns a predicted output in accord with given data
+
+	Arguments:
+	training_set -- training_set to train with
+	num_steps -- num steps to take towards minimum error before returning solution
+	batch -- size of batch to train with each time
+	update_rule -- function derivative of cost with weight or step size
+		has to take four arguments: the training set, the function hypothesis, the feature number, and a list of example numbers to train with
+	log_level -- amount of stuff to log
+	log_frequency -- will log every log_frequency steps
+	>>> ts = ml.generate_training_set(lambda x: 4 * x, 10, -10, 10, 0)
+	>>> function = regression.linear(ts)
+	>>> function([2])
+	7.999999999999996
+	>>> function([7])
+	27.999999999999986
+	>>> 
+	>>> ts = ml.generate_training_set(lambda x, y: 4 * x + y, 10, -10, 10, 0)
+	>>> function = regression.linear(ts)
+	>>> function([3, 4])
+	15.999999999884412
+	>>> 
+	"""
 	#TODO doesn't handly intercepts properly, padding ones doesn't work
 	#training_set.pad_ones()
 	params = [0] * training_set.get_num_features()
 
-	error_plot = []
-
 	hypothesis = lambda x: ml.mult_sum(params, x)
 	for step in range(num_steps):
+
+		#get a batch of random training examples to train with
+		#TODO there are probably better ways of doing this
+		examples = []
+		for d in range(batch):
+			examples.append(randint(0, training_set.size() - 1))
+
 		for i in range(training_set.get_num_features()):
-			#multiply by mean of correct feature
-			grad = ml.gradient(hypothesis, training_set, i, batch_size)
-			diff = (grad * step_size * ml.mean(training_set.get_plottable_input(i)))
-
-			if step % log_frequency == 0:
-				ml.log("Step " + str(step) + ": " + str(diff), ml.V_VERBOSE, log_level)
-
-			params[i] -= diff
+			#update gradient
+			params[i] -= update_rule(training_set, hypothesis, i, examples)
 
 		hypothesis = lambda x: ml.mult_sum(params, x)
+
+		#log stuff
 		if step % log_frequency == 0:
 			ml.log("Error " + str(step) + ": " + str(lms_error(training_set, hypothesis)), ml.V_VERBOSE, log_level)
 		if step % log_frequency == 0:
 			ml.log("Hypothesis " + str(params), ml.V_VERBOSE, log_level)
-		if plot_error != "":
-			error_plot.append(lms_error(training_set, hypothesis))
-		step += 1
-	if plot_error != "":
-		ml.plot_list(range(num_steps), error_plot, plot_error)	
+
 	return hypothesis
